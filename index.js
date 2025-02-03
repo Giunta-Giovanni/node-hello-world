e4c0b28eb6c3f4e7bb21258c9f5e6846eb9f489d
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-03 18:11:02 +0100

    [feat] add package chalk

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello boolean')
+console.log('Caro studente di boolean')
+console.log('volevo dirti che')
+console.log('la variabile non è un dato.')
+console.log('Un dato puo essere:')
+console.log('Un numero')
+console.log('Una stringa')
+console.log('Un valore booleano')
+console.log('undefined')
+console.log('null')
+console.log('una funzione')
+console.log("un'array")
+console.log("un oggetto")
+
+
+
+
+
+
+
+
+
+
+
