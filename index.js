ffe4d9045bfaea719cb9a76c417a2844cefc3911
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-03 19:08:02 +0100

    [feat] console.log boolean with chalk

--- index.js
+++ index.js
@@ -2,41 +2,6 @@ import chalk from 'chalk';
 
 console.log(chalk.magenta('Hello boolean'));
 
-console.log('Nel cuore del codice, i dati si celano,');
-console.log('strutture invisibili che il mondo rivelano.');
-console.log(chalk.red('La variabile li ospita, ma non li definisce,'));
-console.log('è solo un contenitore che il valore estisce.');
-console.log('');
-
-console.log('Esistono', chalk.green('numeri'), 'interi e reali,');
-console.log('precisi o infiniti, tra calcoli astrali.');
-console.log('Le', chalk.green('stringhe'), 'raccontano, senza confusione,');
-console.log('tra apici e doppi, con chiara intenzione.');
-console.log('');
-
-console.log('I', chalk.green('booleani'), 'decidono, sono logica pura,');
-console.log('vero o falso, senza misura.');
-console.log('Un', chalk.green('simbolo'), 'è unico, non si può replicare,');
-console.log('nel caos del codice, lo puoi scovare.');
-console.log('');
-
-console.log('Esiste', chalk.green('null'), 'un’assenza dichiarata,');
-console.log('e', chalk.green('undefined'), 'una strada mai tracciata.');
-console.log('Le', chalk.green('funzioni'), 'esistono, eseguono azioni,');
-console.log('prendono dati e danno soluzioni.');
-console.log('');
-
-console.log('Gli',chalk.green('array'), 'conservano, in ordine sparso,');
-console.log('valori legati, uno spazio mai scarso.');
-console.log('Gli', chalk.green('oggetti'),'creano strutture e relazioni,');
-console.log('chiavi e valori, complesse connessioni.');
-console.log('');
-
-console.log('Ma il dato da solo è privo di scopo,');
-console.log('senza logica e mente che gli dia un moto.');
-console.log('Nel codice risuona la sua esistenza,');
-console.log(chalk.bold('dando forma e vita alla nostra intelligenza.'));
-
 
 
 
